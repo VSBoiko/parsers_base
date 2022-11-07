f4e955d88c6da006307f6f45ebc0d68c8b065d86
Vlad Boiko <vladboiko1996@yandex.ru>
2022-11-07 20:22:30 +0300

    updated comments in file settings.py

--- settings.py
+++ settings.py
@@ -8,17 +8,22 @@ from dotenv import load_dotenv
 dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 
-PRODUCTION = os.getenv("PRODUCTION")                        # флаг включен / выключен боевой режим
+# флаг включен / выключен боевой режим
+PRODUCTION = os.getenv("PRODUCTION")
 if PRODUCTION:
     current_dir = os.path.dirname(os.path.realpath(__file__))
     base_path = os.path.dirname(current_dir)
     sys.path.append(base_path)
     sys.path.append("/home/manage_report")
 
-CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")        # путь к драйверу Chrome
-COOKIES_PATH = os.getenv("COOKIES_PATH")                    # путь к папке с cookies
+# путь к драйверу Chrome
+CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
 
-LOG_FILE_PATH = os.getenv("LOG_FILE_PATH")                  # путь к файлу с логами
+# путь к папке с cookies
+COOKIES_PATH = os.getenv("COOKIES_PATH")
+
+# путь к файлу с логами
+LOG_FILE_PATH = os.getenv("LOG_FILE_PATH")
 
 # заголовки запроса
 HEADERS = {
