2f5be63063b5e16c33eae94a007b6330c6908b3a
vlad boyko <mail@vlaboiko.ru>
2022-11-19 16:06:13 +0300

    created base structure and bae classes

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from Parser import Parser
 
 if __name__ == "__main__":
     parser = Parser(
-        parser_name="tenders.mts.ru",
-        is_sleeping=False,
+        parser_name="site_name",
+        is_sleeping=True,
         is_sending_orders=False,
         is_updating_order=False,
         is_parsing_site=False,
